2496dd520a8b6ad527b8c7fcb01da8d3a1026306
theshrish46 <theshrish46@gmail.com>
2023-04-11 20:39:41 +0530

    worked on project design

--- src/app/api/components/Project.jsx
+++ src/app/api/components/Project.jsx
@@ -16,11 +16,11 @@ const Project = () => {
               <div className="top-left">01.</div>
               <div className="top-right">Featured</div>
             </div>
-            
+
             <div className="mid">
               Kaploom.
             </div>
-            
+
             <div className="down">
               <div className="down-left">Launch Project</div>
               <div className="down-right"><BsArrowUpRight /></div>
@@ -29,7 +29,16 @@ const Project = () => {
         </div>
 
         <div className="projects">
-            
+          <div className="project-top">
+            <div className="one"></div>
+            <div className="two"></div>
+          </div>
+
+          <div className="project-down">
+            <div className="three"></div>
+            <div className="four"></div>
+
+          </div>
         </div>
       </div>
     </div>
